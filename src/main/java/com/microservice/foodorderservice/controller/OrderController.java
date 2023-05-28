001e784d05631f2b865f19a62b83fe757e86ba18
Nethadun Wijesinghe <nethadun123@gmail.com>
2023-05-28 20:51:17 +0530

    order mapping added

--- src/main/java/com/microservice/foodorderservice/controller/OrderController.java
+++ src/main/java/com/microservice/foodorderservice/controller/OrderController.java
@@ -5,17 +5,19 @@ import com.microservice.foodorderservice.dto.request.OrderRequest;
 import com.microservice.foodorderservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/food/item")
+@RequestMapping("/api/food/order")
 @Slf4j
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
+    @PostMapping("/make")
     public CommonResponse makeOrder(@RequestBody OrderRequest orderRequest){
         log.info("OrderController.makeOrder method accessed!");
         return orderService.makeOrder(orderRequest);
